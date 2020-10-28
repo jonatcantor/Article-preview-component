715008f7d3b9d71f7e6a75af8e32567f39f0217b
Jonathan Cantor <jonatcantor@gmail.com>
2020-10-28 15:22:44 -0400

    Compressed css

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ function styles(){
   return gulp.src([
     './src/scss/*.scss'
   ])
-  .pipe(sass().on('error', sass.logError))
+  .pipe(sass({ outputStyle: "compressed" }).on('error', sass.logError))
   .pipe(gulp.dest('./src/css'))
   .pipe(browserSync.stream());
 }
